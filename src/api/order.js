0a4fc39811510f3ad9531e0dd8c8b681900ddc3f
unknown <coolhwm@qq.com>
2017-09-24 21:11:12 +0800

    ADD:完成优惠下单

--- src/api/order.js
+++ src/api/order.js
@@ -1,6 +1,7 @@
-import base from './base'
-import Page from '../utils/Page'
-import wepy from 'wepy'
+import base from './base';
+import Page from '../utils/Page';
+import wepy from 'wepy';
+import Cache from '../utils/Cache';
 
 /**
  * 订单服务类
@@ -17,20 +18,20 @@ export default class order extends base {
     '6': '交易成功',
     '7': '交易关闭',
     '8': '卖家已退款'
-  }
+  };
 
   // 字符字典
   static paymentDict = {
     '0': '线下支付',
     '1': '在线支付'
-  }
+  };
 
   // 订单配送方式
   static deliveryText = {
     'SELF': '上门自提',
     'CITY': '同城配送',
     'EXPRESS': '快递配送'
-  }
+  };
 
   static statusDesc = {
     '1': '请于24小时内付款，超时订单自动关闭',
@@ -41,32 +42,33 @@ export default class order extends base {
     '6': '交易已完成，卖家已收到您的货款',
     '7': '本交易已取消，欢迎您下次光临',
     '8': '钱款已原路退回，请查收'
-  }
+  };
+
   /**
    * 返回分页对象
    */
   static page () {
-    const url = `${this.baseUrl}/orders`
-    return new Page(url, this._processOrderListItem.bind(this))
+    const url = `${this.baseUrl}/orders`;
+    return new Page(url, this._processOrderListItem.bind(this));
   }
 
   /**
    * 获取订单详情
    */
   static getInfo (orderId) {
-    const url = `${this.baseUrl}/orders/${orderId}`
+    const url = `${this.baseUrl}/orders/${orderId}`;
     return this.get(url, {}).then(detail => {
-      this._processOrderDetail(detail)
-      return detail
-    })
+      this._processOrderDetail(detail);
+      return detail;
+    });
   }
 
   /**
    * 生成预支付订单
    */
   static prepayOrder (orderId) {
-    const url = `${this.baseUrl}/wxpay/orders/${orderId}`
-    return this.get(url, {})
+    const url = `${this.baseUrl}/wxpay/orders/${orderId}`;
+    return this.get(url, {});
   }
 
   /**
@@ -79,112 +81,112 @@ export default class order extends base {
       'package': payment.package,
       'signType': 'MD5',
       'paySign': payment.paySign
-    })
+    });
   }
 
   /**
    * 创建订单
    */
   static createOrder (trade, address) {
-    const url = `${this.baseUrl}/orders`
-    this._processOrderAddress(trade, address)
-    return this.post(url, trade)
+    const url = `${this.baseUrl}/orders`;
+    this._processOrderAddress(trade, address);
+    return this.post(url, trade);
   }
 
   /**
    * 申请退款
    */
   static refundOrder (orderId, refund) {
-    const url = `${this.baseUrl}/orders/${orderId}/status/refund`
-    return this.put(url, refund)
+    const url = `${this.baseUrl}/orders/${orderId}/status/refund`;
+    return this.put(url, refund);
   }
 
   /**
    *  取消退款
    */
   static cancelRefund (orderId, refundUuid) {
-    const url = `${this.baseUrl}/orders/${orderId}/status/cancel_refund_money`
+    const url = `${this.baseUrl}/orders/${orderId}/status/cancel_refund_money`;
     const param = {
       refundUuid: refundUuid
-    }
-    return this.put(url, param)
+    };
+    return this.put(url, param);
   }
 
   /**
    * 关闭订单
    */
   static closeOrder (orderId) {
-    const url = `${this.baseUrl}/orders/${orderId}/status/close`
+    const url = `${this.baseUrl}/orders/${orderId}/status/close`;
     const param = {
       note: '买家关闭'
-    }
-    return this.put(url, param)
+    };
+    return this.put(url, param);
   }
 
   /**
    * 确认收货
    */
   static confirmOrder (orderId) {
-    const url = `${this.baseUrl}/orders/${orderId}/status/comments`
-    return this.put(url)
+    const url = `${this.baseUrl}/orders/${orderId}/status/comments`;
+    return this.put(url);
   }
 
   /**
    * 评价订单
    */
   static comment (orderId, comments) {
-    const url = `${this.baseUrl}/comments`
-    return this.post(url, comments)
+    const url = `${this.baseUrl}/comments`;
+    return this.post(url, comments);
   }
 
   /**
    * 评价列表
    */
   static commentList (goodsId) {
-    const url = `${this.baseUrl}/comments`
-    return new Page(url, this._processGoodsComment.bind(this))
+    const url = `${this.baseUrl}/comments`;
+    return new Page(url, this._processGoodsComment.bind(this));
   }
 
   /**
    * 评价统计
    */
   static commentCount (goodsId) {
-    const url = `${this.baseUrl}/comments/count?goods_id=${goodsId}`
-    return this.get(url)
+    const url = `${this.baseUrl}/comments/count?goods_id=${goodsId}`;
+    return this.get(url);
   }
 
   /**
    * 处理评价列表数据
    */
   static _processGoodsComment (data) {
-    const comment = {}
-    comment.createTime = data.createTime.substring(0, 10)
-    comment.starArr = [0, 0, 0, 0, 0]
+    const comment = {};
+    comment.createTime = data.createTime.substring(0, 10);
+    comment.starArr = [0, 0, 0, 0, 0];
     for (let i = 0; i < data.star; i++) {
-      comment.starArr[i] = 1
+      comment.starArr[i] = 1;
     }
-    comment.star = data.star
+    comment.star = data.star;
     if (data.customer) {
-      comment.avatar = data.customer.avatarUrl
-      comment.nickName = data.customer.nickName
+      comment.avatar = data.customer.avatarUrl;
+      comment.nickName = data.customer.nickName;
     } else {
-      comment.avatar = '/images/icons/customer.png'
-      comment.nickName = '微信用户'
+      comment.avatar = '/images/icons/customer.png';
+      comment.nickName = '微信用户';
     }
 
-    comment.comment = data.comment
-    return comment
+    comment.comment = data.comment;
+    return comment;
   }
 
   /**
    * 计算支持的物流方式价格（根据商品信息及地址信息）
    */
   static queryPostPrice (address, goodsList) {
-    const url = `${this.baseUrl}/delivery`
+    const url = `${this.baseUrl}/delivery`;
     const param = {
       address: address,
       goodsList: goodsList
-    }
+    };
     return this.post(url, param).then(data => {
       if (data.delilveryList && data.delilveryList.length > 0) {
         data.delilveryList.forEach(item => {
@@ -200,12 +202,12 @@ export default class order extends base {
   /**
    * 购物车下单
    */
-  static createCartTrade (goodsList) {
-    const orderGoodsInfos = []
-    let price = 0
+  static createCartTrade (goodsList, param) {
+    const orderGoodsInfos = [];
+    let price = 0;
     // 根据购物车信息，构造订单的商品列表
     for (let i in goodsList) {
-      const goods = goodsList[i]
+      const goods = goodsList[i];
       const info = {
         goodsId: goods.goodsId,
         goodsName: goods.goodsName,
@@ -214,29 +216,37 @@ export default class order extends base {
         count: goods.goodsNum,
         skuText: goods.skuText,
         goodsSku: goods.goodsSku
-      }
-      orderGoodsInfos.push(info)
-      price += goods.goodsPrice * goods.goodsNum
+      };
+      orderGoodsInfos.push(info);
+      price += goods.goodsPrice * goods.goodsNum;
     }
+    let finalPrice = price;
+    let reduceFee = 0;
+    if (param && param.reduce) {
+      reduceFee = param.reduce.fee;
+      finalPrice -= reduceFee;
+    }
+    finalPrice = finalPrice.toFixed(2);
     // 构造交易对象
     return {
       dealPrice: price.toFixed(2),
-      finalPrice: price.toFixed(2),
+      reduceFee: reduceFee,
+      finalPrice: finalPrice,
       paymentType: '1',
       paymentText: '在线支付',
       orderGoodsInfos: orderGoodsInfos,
       shopName: this.shopName
-    }
+    };
   }
 
   /**
    * 构建一个交易对象（单个物品），商品页面直接下单
    */
   static createSingleTrade (goods, num = 1, sku) {
-    const imageUrl = this._processSingleOrderImageUrl(goods, sku)
-    const skuText = this._processOrderSku(sku.skuText)
-    const price = sku && sku.price ? sku.price : goods.sellPrice
-    const dealPrice = this._fixedPrice(price * num)
+    const imageUrl = this._processSingleOrderImageUrl(goods, sku);
+    const skuText = this._processOrderSku(sku.skuText);
+    const price = sku && sku.price ? sku.price : goods.sellPrice;
+    const dealPrice = this._fixedPrice(price * num);
     // 构造交易对象
     return {
       dealPrice: dealPrice,
@@ -255,7 +265,7 @@ export default class order extends base {
         }
       ],
       shopName: this.shopName
-    }
+    };
   }
 
   /**
@@ -269,7 +279,7 @@ export default class order extends base {
       contactName: order.receiveName,
       contactPhone: order.receivePhone,
       price: order.finalPrice
-    }
+    };
   }
 
   /**
@@ -277,61 +287,61 @@ export default class order extends base {
    */
 
   static createOrderRefundSetps (refund) {
-    let steps = []
+    let steps = [];
 
     // 提交申请
-    const creareTime = refund.createTime
+    const creareTime = refund.createTime;
     if (creareTime) {
-      steps.push(this._createRefundSetp('您的取消申请已提交，请耐心等待', creareTime))
-      steps.push(this._createRefundSetp('等待卖家处理中,卖家24小时未处理将自动退款', creareTime))
+      steps.push(this._createRefundSetp('您的取消申请已提交，请耐心等待', creareTime));
+      steps.push(this._createRefundSetp('等待卖家处理中,卖家24小时未处理将自动退款', creareTime));
     }
 
     // 卖家处理
-    const sellerTime = refund.sellerDealtime
+    const sellerTime = refund.sellerDealtime;
     if (sellerTime) {
       // 卖家同意
       if (refund.isAgree == 1) {
-        steps.push(this._createRefundSetp('卖家已同意退款', sellerTime))
-        steps.push(this._createRefundSetp('款项已原路退回中，请注意查收', sellerTime))
+        steps.push(this._createRefundSetp('卖家已同意退款', sellerTime));
+        steps.push(this._createRefundSetp('款项已原路退回中，请注意查收', sellerTime));
       } else {
         // 卖家不同意
-        steps.push(this._createRefundSetp(`卖家不同意退款，原因：${refund.disagreeCause}`, sellerTime))
+        steps.push(this._createRefundSetp(`卖家不同意退款，原因：${refund.disagreeCause}`, sellerTime));
       }
     }
 
     // 处理结束
-    const finishTime = refund.finishTime
+    const finishTime = refund.finishTime;
     if (finishTime) {
       // 卖家同意
       if (refund.isAgree == 1) {
-        steps.push(this._createRefundSetp('退款成功', finishTime))
+        steps.push(this._createRefundSetp('退款成功', finishTime));
       } else {
         // 卖家不同意
-        steps.push(this._createRefundSetp('退款关闭，请联系卖家处理', finishTime))
+        steps.push(this._createRefundSetp('退款关闭，请联系卖家处理', finishTime));
       }
     }
 
     // 买家关闭
-    const closeTime = refund.closeTime
+    const closeTime = refund.closeTime;
     if (closeTime) {
       // 卖家同意
       if (refund.isAgree == 2) {
-        steps.push(this._createRefundSetp('退款关闭，请联系卖家处理', finishTime))
+        steps.push(this._createRefundSetp('退款关闭，请联系卖家处理', finishTime));
       } else if (refund.isAgree == 1) {
         // 不需要
       } else {
-        steps.push(this._createRefundSetp('买家取消退款，交易恢复', closeTime))
+        steps.push(this._createRefundSetp('买家取消退款，交易恢复', closeTime));
       }
     }
 
     // 改变最后一个状态
-    const lastStep = steps[steps.length - 1]
-    lastStep.done = true
-    lastStep.current = true
+    const lastStep = steps[steps.length - 1];
+    lastStep.done = true;
+    lastStep.current = true;
 
     // 反转
-    steps = steps.reverse()
-    return steps
+    steps = steps.reverse();
+    return steps;
   }
 
   static _createRefundSetp (text, time) {
@@ -340,7 +350,7 @@ export default class order extends base {
       timestape: time,
       done: false,
       current: false
-    }
+    };
   }
 
   /** ********************* 数据处理方法 ***********************/
@@ -350,10 +360,10 @@ export default class order extends base {
    */
   static _processSingleOrderImageUrl (goods, seletedSku) {
     if (seletedSku && seletedSku.imageUrl) {
-      return seletedSku.imageUrl
+      return seletedSku.imageUrl;
     } else {
-      const hasImage = goods.images && goods.images.length > 0
-      return hasImage ? goods.images[0].url : null
+      const hasImage = goods.images && goods.images.length > 0;
+      return hasImage ? goods.images[0].url : null;
     }
   }
 
@@ -362,28 +372,28 @@ export default class order extends base {
    */
   static _processOrderAddress (order, address) {
     order.receiveName = `${address.name} ${address.sexText}`;
-    order.receivePhone = address.phone
-    order.address = address.fullAddress
+    order.receivePhone = address.phone;
+    order.address = address.fullAddress;
   }
 
   /**
    * 处理订单列表数据
    */
   static _processOrderListItem (order) {
-    const status = order.status
-    order.statusText = this.statusDict[status]
+    const status = order.status;
+    order.statusText = this.statusDict[status];
     // 动作控制 待付款/待评论/待收货/待发货（线下）
-    order.isAction = status == 1 || status == 3 || status == 4
+    order.isAction = status == 1 || status == 3 || status == 4;
     if (order.paymentType == 0 && order.status == 2) {
-      order.isAction = true
+      order.isAction = true;
     }
 
-    order.shopName = this.shopName
+    order.shopName = this.shopName;
     // 处理订单价格
-    this._processOrderPrice(order)
+    this._processOrderPrice(order);
     // 处理商品信息
-    const goods = order.orderGoodsInfos
-    this._processOrderGoods(goods)
+    const goods = order.orderGoodsInfos;
+    this._processOrderGoods(goods);
   }
 
   /**
@@ -391,55 +401,56 @@ export default class order extends base {
    */
   static _processOrderDetail (detail) {
     // 支付方式
-    detail.shopName = this.shopName
+    detail.shopName = this.shopName;
     // 处理订单支付方式
-    this._processOrderPaymentText(detail)
+    this._processOrderPaymentText(detail);
     // 处理订单状态
-    this._processOrderStatusDesc(detail)
+    this._processOrderStatusDesc(detail);
     // 处理退款信息
-    this._processOrderRefund(detail)
+    this._processOrderRefund(detail);
     // 处理物流信息
-    this._processOrderTrace(detail)
+    this._processOrderTrace(detail);
     // 处理订单配送方式
-    this._processOrderDetailDelivery(detail)
+    this._processOrderDetailDelivery(detail);
     // 处理订单价格
-    this._processOrderPrice(detail)
+    this._processOrderPrice(detail);
     // 处理商品信息
-    this._processOrderGoods(detail.orderGoodsInfos)
+    this._processOrderGoods(detail.orderGoodsInfos);
   }
 
   /**
    * 处理订单支付方式
    */
   static _processOrderPaymentText (detail) {
-    detail.paymentText = this.paymentDict[detail.paymentType]
+    detail.paymentText = this.paymentDict[detail.paymentType];
   }
 
   /**
    * 处理订单状态
    */
   static _processOrderPrice (order) {
-    order.postFee = this._fixedPrice(order.postFee)
-    order.dealPrice = this._fixedPrice(order.dealPrice)
-    order.finalPrice = this._fixedPrice(order.finalPrice)
-    order.couponPrice = this._fixedPrice(order.couponPrice)
+    order.postFee = this._fixedPrice(order.postFee);
+    order.dealPrice = this._fixedPrice(order.dealPrice);
+    order.finalPrice = this._fixedPrice(order.finalPrice);
+    order.couponPrice = this._fixedPrice(order.couponPrice);
+    order.reduceFee = this._fixedPrice(order.reduceFee);
   }
 
   static _fixedPrice (price) {
     if (price == null || isNaN(Number(price))) {
-      return null
+      return null;
     }
-    return price.toFixed(2)
+    return price.toFixed(2);
   }
 
   /**
    * 处理状态描述文本
    */
   static _processOrderStatusDesc (order) {
-    const status = order.status
-    order.statusText = this.statusDict[status]
-    order.statusDesc = this.statusDesc[status]
-    order.isAction = status == 1 || status == 2 || status == 3 || status == 4
+    const status = order.status;
+    order.statusText = this.statusDict[status];
+    order.statusDesc = this.statusDesc[status];
+    order.isAction = status == 1 || status == 2 || status == 3 || status == 4;
   }
 
   /**
@@ -447,41 +458,41 @@ export default class order extends base {
    */
   static _processOrderDetailDelivery (order) {
     // const price = order.postFee == 0 ? '免邮' : '￥' + order.postFee;
-    order.deliveryText = this.deliveryText[order.deliveryType]
+    order.deliveryText = this.deliveryText[order.deliveryType];
   }
 
   /**
    * 处理商品物流信息
    */
   static _processOrderTrace (order) {
-    const express = order.orderExpress
+    const express = order.orderExpress;
     if (express == null) {
       // 没有物流信息，不做处理
-      return
+      return;
     }
 
     // 有物流，就一定需要展现动作列表
-    order.isAction = true
-    order.isExpress = true
+    order.isAction = true;
+    order.isExpress = true;
   }
 
   /**
    * 处理订单的退货信息
    */
   static _processOrderRefund (order) {
-    const refunds = order.orderRefunds
+    const refunds = order.orderRefunds;
     if (refunds == null || refunds.length < 1) {
       // 订单没有退款信息，不做处理
-      return
+      return;
     }
     // 展现第一个退款记录
-    const refund = refunds[refunds.length - 1]
+    const refund = refunds[refunds.length - 1];
     // 曾经退款过，就一定需要展现退款记录
-    order.isAction = true
+    order.isAction = true;
     // 控制展现退款详情字段
-    order.isRefund = true
+    order.isRefund = true;
     // 取出第一条退款记录
-    order.curRefund = refund
+    order.curRefund = refund;
   }
 
   /**
@@ -489,16 +500,16 @@ export default class order extends base {
    */
   static _processOrderGoods (goods) {
     goods.forEach(item => {
-      item.imageUrl += '/small'
-    })
+      item.imageUrl += '/small';
+    });
     if (goods == null || goods.length < 1) {
-      return
+      return;
     }
     goods.forEach(item => {
       // 处理SKU描述
-      const sku = item.goodsSku
-      item.skuText = this._processOrderSku(sku)
-    })
+      const sku = item.goodsSku;
+      item.skuText = this._processOrderSku(sku);
+    });
   }
 
   /**
@@ -506,10 +517,10 @@ export default class order extends base {
    */
 
   static _processOrderSku (goodsSku) {
-    let skuText = ''
+    let skuText = '';
     if (goodsSku && goodsSku != '') {
-      skuText = goodsSku.replace(/:/g, ',')
+      skuText = goodsSku.replace(/:/g, ',');
     }
-    return skuText
+    return skuText;
   }
 }
