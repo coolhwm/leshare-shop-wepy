20a88576f6a40e24bf2ee865001244a296d20024
mmmmg <2291918627@qq.com>
2017-11-04 13:09:23 +0800

    ADD:修订会员卡

--- src/api/member.js
+++ src/api/member.js
@@ -4,15 +4,16 @@ export default class Member extends base {
     /**
      * 获取会员卡信息
      */
-    static async info() {
-        const url = `${this.baseUrl}/members`;
-        return await this.get(url);
-    }
+  static async info() {
+    const url = `${this.baseUrl}/members`;
+    return await this.get(url);
+  }
+
     /**
      * 注册会员卡信息
      */
-    static async create(param) {
-        const url = `${this.baseUrl}/members`;
-        return this.post(url,param);
-    }
+  static async create(param) {
+    const url = `${this.baseUrl}/members`;
+    return this.post(url, param);
+  }
 }
