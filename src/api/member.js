818a3bc9b83584a08c5d1c4d09a9b5d50e10576d
mmmmg <2291918627@qq.com>
2017-11-10 11:43:05 +0800

    ADD:历史积分头像不显示

--- src/api/member.js
+++ src/api/member.js
@@ -1,4 +1,5 @@
 import base from './base';
+import Page from '../utils/Page';
 
 export default class Member extends base {
     /**
@@ -15,5 +16,12 @@ export default class Member extends base {
   static async create(param) {
     const url = `${this.baseUrl}/members`;
     return this.post(url, param);
+  }
+    /**
+     * 历史积分信息
+     */
+  static async bonusPage() {
+    const url = `${this.baseUrl}/members/bonus_detail`;
+    return new Page(url);
   }
 }
