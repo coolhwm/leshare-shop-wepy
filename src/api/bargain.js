9b05acc4c87a4d396c067f4196de4cfbe184d31d
lin19950219 <779897971@qq.com>
2018-03-14 21:04:56 +0800

    ADD：砍价详情本人

--- src/api/bargain.js
+++ src/api/bargain.js
@@ -1,6 +1,7 @@
 import base from './base';
 import api from './group';
 import Page from '../utils/Page';
+import wepy from 'wepy';
 
 export default class group extends base {
   /***
@@ -14,11 +15,12 @@ export default class group extends base {
   /***
    * 开砍/帮砍
    */
-  static GoodsBargain (ruleId, id) {
+  static GoodsBargain (ruleId, id, sku) {
     const url = `${this.baseUrl}/goods_bargain`;
     const param = {
       ruleId: ruleId,
-      id: id
+      id: id,
+      sku: sku
     };
     return this.post(url, param)
   }
@@ -32,7 +34,56 @@ export default class group extends base {
 
   // 处理方法
   static _processBargainGoods (data) {
-    api._processGoodsPreview(data.rule)
+
+    // 处理预览图
+    api._processGoodsPreview(data.rule);
+
+    // 筛选规格
+    this._processSkuDetail(data);
+
+    // 筛选开砍者
+    this._processBargainHeader(data);
+
+    // 求一共砍了多少钱
+    this._processAllBargainPrice(data);
+
+    // 求还剩多少钱
+    this._processBalance(data);
     return data;
+  }
+
+  // 筛选开砍规格
+  static _processSkuDetail (data) {
+    data.rule.skuDetail = data.rule.skuDetails.find(item => item.sku === data.sku)
+  }
+
+  // 筛选开砍者
+  static _processBargainHeader (data) {
+    // const user = wepy.getStorageSync('user');
+    data.header = data.details[0];
+    // data.isHead = user.id === data.header.customer.id;
+    data.isHead = false;
+  }
+
+  // 求一共砍了多少钱
+  static _processAllBargainPrice (data) {
+    const prices = data.details.map(item => item.reducePrice);
+    data.allPrice = 0;
+    let max = 0;
+    for (let idx in prices) {
+      data.allPrice += prices[idx];
+      if (prices[idx] > max) {
+        max = prices[idx];
+      }
+    }
+  }
+
+  // 求还剩多少钱
+  static _processBalance (data) {
+    if (data.allPrice) {
+      data.balance = (data.rule.skuDetail.price * 1 - data.allPrice * 1).toFixed(2);
+    } else {
+      data.balance = data.rule.skuDetail.price * 1;
+    }
   }
 }
