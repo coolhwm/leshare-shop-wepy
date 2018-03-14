44dbff892d83fd31c585f77da1e868b265967721
lin19950219 <779897971@qq.com>
2018-03-14 17:19:34 +0800

    ADD:砍价详情页

--- src/api/bargain.js
+++ src/api/bargain.js
@@ -10,4 +10,29 @@ export default class group extends base {
     const url = `${this.baseUrl}/goods_bargain/rules/${ruleId}`;
     return this.get(url).then(data => api._processGoodsDetail(data));
   }
+
+  /***
+   * 开砍/帮砍
+   */
+  static GoodsBargain (ruleId, id) {
+    const url = `${this.baseUrl}/goods_bargain`;
+    const param = {
+      ruleId: ruleId,
+      id: id
+    };
+    return this.post(url, param)
+  }
+  /***
+   * 根据砍价ID查找砍价详情
+   */
+  static bargain (bargainId) {
+    const url = `${this.baseUrl}/goods_bargain/${bargainId}`;
+    return this.get(url).then(data => this._processBargainGoods(data));
+  }
+
+  // 处理方法
+  static _processBargainGoods (data) {
+    api._processGoodsPreview(data.rule)
+    return data;
+  }
 }
