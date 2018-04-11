1b00dd8d25edc4ee48d77e6de9dbe34ad4940b1c
lin19950219 <779897971@qq.com>
2018-04-11 17:58:59 +0800

    ADD:虚拟商品优惠券与订单之间的跳转及我的邀请页面动态加载数据

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -98,6 +98,15 @@ export default class coupon extends base {
     return this.post(url, visit);
   }
   /**
+   * 根据id查询卡卷详情
+   */
+  static detail(id) {
+    const url = `${this.baseUrl}/coupons/info/${id}`;
+    return this.get(url).then(data => {
+      return this._processCouponItem(data)
+    });
+  }
+  /**
    * 处理可以领取的优惠券
    */
   static _processPickItem (coupon) {
@@ -115,6 +124,7 @@ export default class coupon extends base {
     }
     const coupon = data.coupon;
 
+    coupon.orderId = root.orderId;
     coupon.status = root.status;
     coupon.id = root.id;
     coupon.couponId = root.couponId;
