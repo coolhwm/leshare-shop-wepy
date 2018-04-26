6a22b14bccabf9213e37235c679dc01118a9595c
lin19950219 <779897971@qq.com>
2018-04-26 18:11:15 +0800

    ADD:充值与分销申请

--- src/api/agent.js
+++ src/api/agent.js
@@ -1,15 +1,20 @@
 import base from './base';
 
-/**
- * 商品服务类
- */
 export default class goods extends base {
   /**
-   * 查询商品目录
+   * 申请
    */
   static apply (param) {
     const url = `${this.baseUrl}/agent/apply`;
     return this.post(url, param);
+  }
+
+  /***
+   * 查询
+   */
+  static agent (customerId) {
+    const url = `${this.baseUrl}/agent/agent/${customerId}`;
+    return this.get(url);
   }
 
   /** ********************* 数据处理方法 ***********************/
