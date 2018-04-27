edfa47ef5c81fdc51aab1aadb6e74f0b7c96d115
lin19950219 <779897971@qq.com>
2018-04-27 16:29:55 +0800

    ADD:分销团队列表

--- src/api/agent.js
+++ src/api/agent.js
@@ -1,4 +1,6 @@
 import base from './base';
+import api from "./group";
+import Page from "../utils/Page";
 
 export default class goods extends base {
   /**
@@ -10,12 +12,38 @@ export default class goods extends base {
   }
 
   /***
-   * 查询
+   * 查询个人分销信息
    */
-  static agent (customerId) {
-    const url = `${this.baseUrl}/agent/agent/${customerId}`;
-    return this.get(url);
+  static agentInfo () {
+    const url = `${this.baseUrl}/agent`;
+    return this.get(url).then(data => this._processAgent(data));
+  }
+
+  /***
+   * 查看分销员子代理列表
+   */
+  static agentGroups (agentId) {
+    const url = `${this.baseUrl}/agent/list/${agentId}`;
+    return new Page(url);
+  }
+
+  /***
+   * 查看分销员子代理列表
+   */
+  static agentDetail (agentId) {
+    const url = `${this.baseUrl}/agent/details/${agentId}`;
+    return new Page(url);
   }
 
   /** ********************* 数据处理方法 ***********************/
+  static _processAgent(data) {
+    // 处理分销状态
+    const AGENT_STATUS = {
+      AUDITTING: '待审核',
+      ENABLE: '生效中',
+      DISABLE: '已失效'
+    };
+    data.statusText = AGENT_STATUS[data.status];
+    return data;
+  }
 }
