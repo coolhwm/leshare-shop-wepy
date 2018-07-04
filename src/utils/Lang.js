1352a73f8a2c736e7e793c3c14a0e6c70f77ff7b
coolhwm <coolhwm@qq.com>
2018-07-04 17:07:36 +0800

    ADD:防止重复下单

--- src/utils/Lang.js
+++ src/utils/Lang.js
@@ -67,4 +67,13 @@ export default class Lang {
     }
     return price.toFixed(2);
   }
+
+  static guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
 }
